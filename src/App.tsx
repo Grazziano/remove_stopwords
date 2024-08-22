703d5fc7b987edad5d73fd3913f92860ed7d144f
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-08-22 20:38:17 -0300

    feat: Remove Stopwords and Normalize Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { stopwords } from './utils/stopwords';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState<string>('');
+  const [processedText, setProcessedText] = useState<string>('');
+
+  function removeStopwords(text: string) {
+    return text
+      .split(' ')
+      .filter((word) => !stopwords.includes(word.toLowerCase()))
+      .join(' ');
+  }
+
+  function normalizeText(text: string) {
+    return text.toLowerCase();
+  }
+
+  function processText() {
+    const newNormalizeText = normalizeText(text);
+    const textWithoutStopwords = removeStopwords(newNormalizeText);
+
+    setProcessedText(textWithoutStopwords);
+    console.log(textWithoutStopwords);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Remover Stopwords e Normalizar Texto</h1>
+
+      <textarea
+        id="inputText"
+        rows={10}
+        cols={50}
+        placeholder="Cole seu texto aqui..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <br />
+      <input type="file" id="fileInput" />
+      <br />
+      <button onClick={processText}>Processar Texto</button>
+
+      <h2>Texto Processado:</h2>
+
+      <br />
+      <span>{processedText}</span>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/stopwords.ts
@@ -0,0 +1,16 @@
+export const stopwords = [
+  "a", "à", "ainda", "além", "algo", "alguém", "algum", "alguma", "alguns", "ampla", "amplas", "amplo",
+  "amplos", "ante", "antes", "ao", "aos", "após", "aquela", "aquelas", "aquele", "aqueles", "aquilo",
+  "as", "até", "atrás", "cada", "coisa", "com", "como", "contra", "contudo", "da", "daquele", "daqueles",
+  "das", "de", "dela", "delas", "dele", "deles", "demais", "depois", "desde", "desta", "deste", "disse",
+  "disso", "disto", "dito", "diz", "dizer", "do", "dos", "e", "é", "ela", "elas", "ele", "eles", "em",
+  "entre", "era", "essa", "essas", "esse", "esses", "esta", "estas", "estava", "este", "estes", "eu",
+  "fazer", "foi", "foram", "fosse", "grande", "grandes", "há", "isso", "isto", "já", "lá", "lhe", "lhes",
+  "mais", "mas", "me", "mesmo", "meu", "meus", "minha", "minhas", "muito", "na", "não", "nas", "nem",
+  "no", "nos", "nós", "nossa", "nossas", "nosso", "nossos", "num", "numa", "nunca", "o", "os", "ou",
+  "outra", "outras", "outro", "outros", "para", "pela", "pelas", "pelo", "pelos", "perante", "pode",
+  "pode", "pude", "pois", "por", "porque", "portanto", "quais", "qual", "qualquer", "quando", "quanto",
+  "que", "quem", "quer", "se", "seja", "sem", "seu", "seus", "só", "sob", "sobre", "sua", "suas", "talvez",
+  "também", "tão", "tais", "toda", "todas", "todo", "todos", "tudo", "tua", "tuas", "um", "uma", "umas",
+  "uns", "vou", "à", "às", "é", "ó", "os", "um", "uma", "você", "vocês"
+];
