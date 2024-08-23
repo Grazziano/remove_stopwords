b5d4f17222fe7569a6118bb9317a23893d105d99
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-08-23 13:37:42 -0300

    feat: implementing light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useState } from 'react';
 import './App.css';
 import { removeStopwords, eng, porBr } from 'stopword';
-// import { stopwordsEN, stopwordsPT } from './utils/stopwords';
+import { Sun, MoonStar } from 'lucide-react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -9,6 +9,7 @@ function App() {
   const [text, setText] = useState<string>('');
   const [language, setLanguage] = useState<'pt' | 'en'>('pt');
   const [normalizedText, setNormalizedText] = useState<string>('');
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
 
   const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
     setText(e.target.value);
@@ -31,7 +32,6 @@ function App() {
 
   const normalizeText = (): void => {
     const words = text.split(/\s+/);
-    // const stopwords = language === 'pt' ? stopwordsPT : stopwordsEN;
     const normalizedWords = removeStopwords(
       words,
       language === 'pt' ? porBr : eng
@@ -63,51 +63,62 @@ function App() {
     });
   };
 
-  return (
-    <div className="max-w-2xl mx-auto p-4 bg-white shadow-lg rounded-lg">
-      <h1 className="text-2xl font-bold mb-4">
-        Remover Stopwords e Normalizar Texto
-      </h1>
-      <textarea
-        rows={10}
-        className="w-full border border-gray-300 rounded-md p-2 mb-4"
-        value={text}
-        onChange={handleTextChange}
-        placeholder="Cole seu texto aqui..."
-      />
-      <div className="mb-4">
-        <input
-          type="file"
-          onChange={handleFileChange}
-          accept=".txt"
-          className="border border-gray-300 rounded-md p-2"
-        />
-      </div>
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
 
-      <div className="flex justify-between items-center">
-        <div className="mb-4">
-          <label className="block font-medium mb-2">
-            Idioma:
-            <select
-              value={language}
-              onChange={handleLanguageChange}
-              className="border border-gray-300 rounded-md p-2 ml-2"
-            >
-              <option value="pt">Português</option>
-              <option value="en">Inglês</option>
-            </select>
-          </label>
-        </div>
-        <button
-          onClick={normalizeText}
-          className="bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600 transition duration-200"
-        >
-          Normalizar Texto
+  return (
+    <div className={`h-screen ${theme}`}>
+      <div className="max-w-2xl mx-auto p-4 shadow-lg rounded-lg">
+        <h1 className="text-2xl font-bold mb-4">
+          Remover Stopwords e Normalizar Texto
+        </h1>
+
+        <button onClick={toggleTheme} className="float-end border-none p-2">
+          {theme === 'light' ? <MoonStar /> : <Sun />}
         </button>
+
+        <textarea
+          rows={10}
+          className="w-full border border-gray-300 rounded-md p-2 mb-4"
+          value={text}
+          onChange={handleTextChange}
+          placeholder="Cole seu texto aqui..."
+        />
+        <div className="mb-4">
+          <input
+            type="file"
+            onChange={handleFileChange}
+            accept=".txt"
+            className="border border-gray-300 rounded-md p-2"
+          />
+        </div>
+
+        <div className="flex justify-between items-center">
+          <div className="mb-4">
+            <label className="block font-medium mb-2">
+              Idioma:
+              <select
+                value={language}
+                onChange={handleLanguageChange}
+                className="border border-gray-300 rounded-md p-2 ml-2"
+              >
+                <option value="pt">Português</option>
+                <option value="en">Inglês</option>
+              </select>
+            </label>
+          </div>
+          <button
+            onClick={normalizeText}
+            className="bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600 transition duration-200"
+          >
+            Normalizar Texto
+          </button>
+        </div>
       </div>
 
       {normalizedText && (
-        <>
+        <div className="max-w-2xl mx-auto p-4 shadow-lg rounded-lg">
           <h2 className="text-xl font-semibold mt-6 mb-2">
             Texto Normalizado:
           </h2>
@@ -130,7 +141,7 @@ function App() {
               Exportar Arquivo
             </button>
           </div>
-        </>
+        </div>
       )}
 
       <ToastContainer />
